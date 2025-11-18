00be3a62990ceca0045d94bdae5d793c8a8032b6
Mahesh-m-s <777mahesh.m.s@gmail.com>
2025-11-18 11:27:18 +0800

    calculator

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput,Button} from 'react-native';
 
 export default function App() {
+  const [calcData, setCalcData] = useState({ num: "0", result: null });
+  const docalc = () =>{
+    if(isNaN(calcData.num)){
+      setCalcData({...calcData,result:'Invalid Number'})
+      return;
+    }
+    const num = calcData.num
+    const sqr = num*num;
+    setCalcData({...calcData,result:`Result of ${num} : ${sqr}`})
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>hi! this is calculator app</Text>
+
+      <TextInput 
+        value={calcData.num}
+        keyboardType='numeric'
+        placeholder='Enter number'
+        style={styles.input}
+        onChangeText={(data) => setCalcData({ ...calcData, num: data })}/>
+        <Button title = "Find sqaure" onPress ={docalc}/>
+        <Text>{calcData.result}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#27d310fb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  input: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 18,
+  }
 });
